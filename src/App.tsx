83f74ae254bf8e26c2091202125bf3a157cfc381
Pablo Baldini <51711014+baldipa3@users.noreply.github.com>
2025-09-12 07:37:53 +0200

    add container to fix Toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import AppRoutes from "./routes";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+      <ToastContainer position="top-right" autoClose={3000} />
+    </>
+  );
 }
 
 export default App;
